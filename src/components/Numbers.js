a88de2e4b4b3083808d7409f01bf95e77a627200
Anna Kharitonchik <zharitonchik@icloud.com>
2025-09-10 12:07:41 +0300

    feat:  add click on numbers

--- src/components/Numbers.js
+++ src/components/Numbers.js
@@ -1,4 +1,4 @@
-export const Numbers = () => {
+export const Numbers = (state) => {
   const numField = document.createElement("div");
   const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
@@ -6,6 +6,13 @@ export const Numbers = () => {
     const numButton = document.createElement("button");
     numButton.textContent = num;
     numField.appendChild(numButton);
+    numButton.addEventListener("click", () => {
+      if (state.currentNum === "0" || state.currentNum === "Error") {
+        state.currentNum = num.toString();
+      } else {
+        state.currentNum += num.toString();
+      }
+    });
   });
   return numField;
 };
